ca15f6e526136d997048aa0a85fcfcad5e235423
Imre Lorvi <imre.lorvi@gmail.com>
2018-01-21 05:35:56 +0200

    apply automatic formatting

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/exception/UserNotLoggedInException.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/exception/UserNotLoggedInException.java
@@ -2,6 +2,5 @@ package org.craftedsw.tripservicekata.exception;
 
 public class UserNotLoggedInException extends RuntimeException {
 
-	private static final long serialVersionUID = 8959479918185637340L;
-
+    private static final long serialVersionUID = 8959479918185637340L;
 }
