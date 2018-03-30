f7d71f31bc3b324e335177506b5f1c484c8bfe35
Imre Lorvi <imre.lorvi@gmail.com>
2018-03-30 16:10:07 +0300

    move hardcoded usersession to its own method

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,8 +11,7 @@ public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         List<Trip> tripList = new ArrayList<Trip>();
-        User loggedUser = UserSession.getInstance()
-                .getLoggedUser();
+        final User loggedUser = this.getLoggedUser();
         boolean isFriend = false;
         if (loggedUser != null) {
             for (User friend : user.getFriends()) {
@@ -28,5 +27,9 @@ public class TripService {
         } else {
             throw new UserNotLoggedInException();
         }
+    }
+
+    protected User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
     }
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,34 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class TripServiceTest {
 
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule()
+            .strictness(Strictness.STRICT_STUBS);
+
+    @Test
+    public void should_throw_when_no_user_session_found() throws Exception {
+        //given
+        final TripService service = new TripService(){
+            @Override
+            protected User getLoggedUser() {
+                return null;
+            }
+        };
+
+        //when/then
+        assertThatExceptionOfType(UserNotLoggedInException.class).isThrownBy(() -> {
+            service.getTripsByUser(null);
+        });
+    }
 }
