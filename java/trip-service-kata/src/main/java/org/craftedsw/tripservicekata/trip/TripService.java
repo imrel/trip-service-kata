1c9b8cc57921d1028ac0c571de72c731803f68e8
Imre Lorvi <imre.lorvi@gmail.com>
2018-04-01 01:26:07 +0300

    no trips should be immutable

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -4,12 +4,12 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TripService {
 
-    private static final ArrayList<Trip> NO_TRIPS = new ArrayList<>();
+    private static final List<Trip> NO_TRIPS = Collections.emptyList();
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         final User loggedUser = this.getLoggedUser();
