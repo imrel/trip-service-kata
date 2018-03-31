5ea58f33c23f2475575d8a32c3d7675b24568ff3
Imre Lorvi <imre.lorvi@gmail.com>
2018-04-01 01:53:28 +0300

    added nonstatic find method to TripDAO

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
@@ -11,4 +11,9 @@ public class TripDAO {
     public static List<Trip> findTripsByUser(User user) {
         throw new CollaboratorCallException("TripDAO should not be invoked on an unit test.");
     }
+
+    public List<Trip> findTripsBy(User user) {
+
+        return findTripsByUser(user);
+    }
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
@@ -1,6 +1,19 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.CollaboratorCallException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 @SuppressWarnings("unused")
 public class TripDAOTest {
 
+    @Test
+    public void should_throw_when_getting_user_trips() throws Exception {
+        //given
+
+        //when/then
+        assertThatExceptionOfType(CollaboratorCallException.class).isThrownBy(() -> new TripDAO().findTripsBy(new User()));
+    }
 }
